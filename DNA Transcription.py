bce5ed1d80b29a50fea01715f209dc841b9ba402
Rogges <roggesanandarajah@gmail.com>
2016-03-26 17:57:29 -0400

    Update and rename transcribe dna to rna.py to DNA Transcription.py

--- DNA Transcription.py
+++ DNA Transcription.py
@@ -1,6 +1,9 @@
 def transcribe(bases):
     bases=list(bases)
     bases1=['U' if base=='T' else base for base in bases]
+    
+    #replaces all T bases for U 
+    
     string=''.join(bases1)
     print(string)
 
