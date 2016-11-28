a0032a69da1118ab3ea5fe573608c6b92bc30997
Rogges Anandarajah <roggesanandarajah@gmail.com>
2016-11-28 16:08:09 -0500

    Update Counting Nucleotide Bases.py

--- Counting Nucleotide Bases.py
+++ Counting Nucleotide Bases.py
@@ -1,13 +1,8 @@
 def basecounter(string):
-  string=list(string)
   
-  #turning the string into a list allows list comprehensions to do the sorting
+  #The counting can be contained within a dictionary comprehension; the iterated string "ACTG" can be modified to include things like 
+  #missing values if need be
   
-  A=[base for base in string if base=='A']
-  C=[base for base in string if base=='C']
-  T=[base for base in string if base=='T']
-  G=[base for base in string if base=='G']
-  
-  #the list comprehensions sort the bases into one of the 4 nucleotide base groupings
-  
-  print(len(A),len(C),len(T),len(G))
+  return {x:string.count(x) for x in "ACTG"}
+
+
