c9779642ad9f92ca505b6269063a8342f3429c0e
Rogges Anandarajah <roggesanandarajah@gmail.com>
2016-11-28 12:23:40 -0500

    Update Reverse Complement.py

--- Reverse Complement.py
+++ Reverse Complement.py
@@ -1,20 +1,9 @@
 def rcomplement(string):
-    bases=list(string)
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
     
-    # turning the string into a list allows for list comprehension manipulations
+    #A list comprehension can be used to call the complement base for each based in the reversed string, then this new list can be joined 
+    #into an empty string
     
-    bases=bases[::-1]
-    
-    # using a -1 slice step means the bases results in the nucleotide bases being reverse ordered
-    
-    bases=['t' if base=='A' else base for base in bases]
-    bases=['A' if base=='T' else base for base in bases]
-    bases=['c' if base=='G' else base for base in bases]
-    bases=['G' if base=='C' else base for base in bases]
-    
-    # each successive base is transformed to its complement base and alternating U.Case and L.Case letters are used so things aren't overwritten
-    
-    bstring=''.join(bases)
-    print(bstring.upper())
+    return ''.join([complement[x] for x in string[::-1]])
 
-    # this returns the list of bases back into the string of bases
+    
