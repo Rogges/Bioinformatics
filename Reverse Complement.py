91db03c954110b73f5787ee29c5e47f240815630
Rogges Anandarajah <roggesanandarajah@gmail.com>
2016-11-28 13:05:06 -0500

    Update Reverse Complement.py

--- Reverse Complement.py
+++ Reverse Complement.py
@@ -1,7 +1,12 @@
 def rcomplement(string):
+    complement={'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
     #A list comprehension can be used to call the complement base for each based in the reversed string, then this new list can be joined 
     #into an empty string
     
-    return ''.join([{'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}[x] for x in string[::-1]])
+    return ''.join([complement[x] for x in string[::-1]])
 
     
+def rcomplement2(string):
+    return ''.join([{'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}[x] for x in string[::-1]])
+
+#rcomplement is much faster than rcomplement2 but I figured it would be fun to have a one line solution to the problem
